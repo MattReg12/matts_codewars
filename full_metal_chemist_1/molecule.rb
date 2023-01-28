4513d98d11a71b728c5b2679e9fa7ef0ef71930e
Matt Register <rmregister0510@gmail.com>
2023-01-28 06:13:50 -0500

    before atom_bond_change

--- full_metal_chemist_1/molecule.rb
+++ full_metal_chemist_1/molecule.rb
@@ -5,35 +5,69 @@ created, all of its atoms are carbons. Each branch of the molecule is ID'd
 by a number that matches its creation order.
 
 
+#methods that involve building molecule objects must be chainable i.e. they need
+to return molecule objects
+
+#building a molecule concosts in mutating the original object
+
+
+valence number of an atom -- the number of bounds it can hold. no more, no less
+
+molecular wieght -- the sum of the atomic weight of all its atoms
+
 =end
 
+class InvalidBond < StandardError
+# throw if you encounter a case where atom exceeeds valence number or is bound to itself
+# when exception thrown while it still has args left to handle, the modifications resulting
+# from previous valid operations must be kept
+end
 
+class LockedMolecule < StandardError
+  #thrown when attempt to modify after locked
+end
 
 
 class Molecule
-    attr_reader :name
+  attr_reader :name
 
   def initialize(name = '')
     @name = name
-    @branches = []
+    @atoms = []
+    @locked = false
+    @atom_count = 0
   end
 
   def formula
-    #gives the raw formula of the final moleculs as a string
+    raise LockedMolecule unless @locked
+
+    ids = @atoms.flatten.map(&:element)
+    counts = ids.tally.to_a
+    counts = counts.map(&:join)
+    counts = counts.partition { |elem| ['H', 'C'].include?(elem[0]) }
+    counts.flatten.join.delete('1')
   end
 
   def moelecular_weight
     # returns a double value in g/mol
+    #throws error if molecule unlocked
+    @atoms.flatten.map(&:weight).sum
   end
 
   def atoms
     # gives list of atoms appended order of their creation
   end
 
-  def brancher(*ints)
-    #postive integers
-    #adds new 'branches' to the current molecule
-    #each arg gives the number of carbons on the new brach
+  # Adds new 'branches' to the current molecule.
+  # Args are the new carbon elements to be added
+  def brancher(*carbons)
+    carbons.each do |carbon|
+      branch = []
+      carbon.times { branch << add_atom('C') }
+      @atoms << branch
+    end
+    bond_carbons
+    self
   end
 
   def bounder(*arrs)
@@ -58,6 +92,7 @@ class Molecule
   end
 
   def add_chaining(*args)
+    # if error occurs at any point, all of its atoms have to be removed from the instance
     # (nc, nb, elt1, elt2, ...)
     # adds on the carbon (nc) in the branch (nb) a chaing with all the provide elements in the specified
     # order. m.add_chaining(2, 5, "N", "C", "C", "Mg", "Br") will add the chain ...
@@ -66,22 +101,92 @@ class Molecule
   end
 
   def closer
-    #finalizes molecule instance. adding missing hydrogens and locking the object
+    bond_hydrogens
+    @locked = true
+    self
   end
 
   def unlock
     #makes molecule mutable
     #removes hydrogens and any empty branches
     #throw EmptyMolecule exception if no branches left
-    #
+    #the ids of the remaining atoms must be continous again starting at 1
+    # if you end up with atoms that arent connected to the branches of the unlocked molecule, keep them anyways
+    #must be modifiable again in any manner
   end
 
+  private
+
+  attr_accessor :locked
+  attr_writer :atom_count
+
+  # creates atom to be added to element and increments atom_count counter
+  def add_atom(elem)
+    @atom_count +=1
+    Atom.new(elem, @atom_count)
+  end
+
+  def bond_hydrogens
+    @atoms.each do |branch|
+      branch.size.times do |atom_i|
+        bonds = branch[atom_i].bonds
+        bonds.each_with_index do |bond, i|
+          if bond.nil?
+            h = Atom.new('H')
+            branch << h
+            bonds[i] = h
+          end
+        end
+      end
+    end
+  end
+
+  def bond_carbons
+    @atoms.each_branch do |branch|
+      branch.each_with_index do |carbon, i|
+
+  end
 end
 
-
-
-
+# Instances of this class represent atoms in a specific Molecule
+# instance and the bonds they hold with other Atom instances.
 class Atom
+  WEIGHT =  { 'H' => 1.0, 'B' => 10.8, 'C' => 12.0, 'N' => 14.0, 'O' => 16.0,
+              'F' => 19.0, 'Mg' => 24.3, 'P' => 31.0, 'S' => 32.1,
+              'Cl' => 35.5, 'Br' => 80.0 }
 
+  VALENCE =  { 'H' => 1, 'B' => 3, 'C' => 4, 'N' => 3, 'O' => 2,
+              'F' => 1, 'Mg' => 2, 'P' => 3, 'S' => 2,
+              'Cl' => 1, 'Br' => 1 }
 
+  attr_accessor :element, :id
+  attr_reader :bonds, :weight
+
+  def initialize(elt, id=1)
+    @element = elt
+    @weight = WEIGHT[elt]
+    @bonds = Array.new(VALENCE[elt])
+    @id = id
+  end
+
+  def hash
+    self.id
+  end
+
+  def ==(other)
+    self.id == other.id
+  end
+
+  def eql?(other)
+    self == other
+  end
+
+  def to_s
+    #"Atom(element.id: element1id,element2id,element3id...)". "Atom(C.24: C1,O6,N2,H)"
+    # hydrogens go at the end and dont display its id number
+    # if not bonded to another element, just return the element.id part like "Atom(C.1)"
+  end
 end
+
+methane = Molecule.new("methane").brancher(1).closer()
+p methane.formula
