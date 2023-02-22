6e1e231f2bbce8ba48972e301e0e1d6d785b18cf
Matt Register <rmregister0510@gmail.com>
2023-02-22 09:30:30 -0500

    enable subroutines

--- assembler_interpreter/assembler.rb
+++ assembler_interpreter/assembler.rb
@@ -50,11 +50,37 @@ C:
 def simple_assembler(program)
   p = Program.new(program)
   p.execute
-  p.variables
+  p.output
+end
+
+module Tokenable
+  def remove_comments(program)
+    program.gsub(/\;.*$/, '')
+  end
+
+  def routines(program)
+    program = program.strip
+    program.split("\n\n")
+  end
+
+  def tokenize(routine)
+    routine = routine.split("\n").map { |instruc| instruc.split }
+    routine = remove_commas(routine)
+    routine
+  end
+
+  def remove_commas(routine)
+    routine.each do |instruc|
+      unless instruc.first == 'msg'
+      instruc.join(' ').delete
+    end
+  end
 end
 
 class Program
-  attr_reader :regs
+  include Tokenable
+
+  attr_reader :regs, :output, :main, :subs
 
   def initialize(main)
     @main = parse(main)
@@ -68,7 +94,9 @@ class Program
     end
   end
 
-  private
+  #private
+
+  #attr_accessor :prev_cmp
 
   def assemble(instruction)
     fx, var, arg = instruction
@@ -107,66 +135,97 @@ class Program
     value(reg_one) <=> value(reg_two)
   end
 
-  def jmp(subroutine)
-
+  def call(label)
+    execute(subroutines[label.intern])
   end
 
-  def jne(subroutine)
-
+  def jmp(label)
+    execute(subroutines[label.intern])
   end
 
-  def je(subroutine)
-
+  def jne(label)
+    jmp(label) unless prev_cmp.zero?
   end
 
-  def jge(subroutine)
-
+  def je(label)
+    jmp(label) if prev_cmp.zero?
   end
 
-  def jg(subroutine)
-
+  def jge(label)
+    jmp(label) if prev_cmp >= 0
   end
 
-  def jle(subroutine)
-
+  def jg(label)
+    jmp(label) if prev_cmp.postive?
   end
 
-  def jl(subroutine)
-
+  def jle(label)
+    jmp(label) if prev_cmp <= 0
   end
 
-  def call(subroutine)
-
+  def jl(label)
+    jmp(label) if prev_cmp.negative?
   end
 
   def reg_value(reg)
     regs.fetch(reg, reg.to_i)
   end
 
-  def parse(instructions)
+  def msg_value(msg)
+    regs.fetch(msg, msg)
+  end
 
+  def msg(*args)
+    @output = args.map { |arg| msg_value(arg) }.join
+  end
+
+  def parse(instrux)
+    instrux = remove_comments(instrux)
+    main, *subs = routines(instrux)
+    #subs.map! { |sub| tokenize(sub) }
+    #record(subs)
+    tokenize(main)
+  end
+
+  def record(subs)
+    @subs = {}
+    subs.each do |sub|
+      @subs[sub.first.first] = sub[1..-1]
+    end
   end
 end
 
-  program_mod = "
-  mov   a, 11           ; value1
-  mov   b, 3            ; value2
-  call  mod_func
-  msg   'mod(', a, ', ', b, ') = ', d        ; output
-  end
+program_fibonacci = "
+    mov   a, 8            ; value
+    mov   b, 0            ; next
+    mov   c, 0            ; counter
+    mov   d, 0            ; first
+    mov   e, 1            ; second
+    call  proc_fib
+    call  print
+    end
 
-  ; Mod function
-  mod_func:
-      mov   c, a        ; temp1
-      div   c, b
-      mul   c, b
-      mov   d, a        ; temp2
-      sub   d, c
-      ret
-  "
+    proc_fib:
+        cmp   c, 2
+        jl    func_0
+        mov   b, d
+        add   b, e
+        mov   d, e
+        mov   e, b
+        inc   c
+        cmp   c, a
+        jle   proc_fib
+        ret
 
-x = program_mod.gsub(/\;.*$/, '')
-main, *fxs = x.strip.split("\n\n").map { |y| y.split("\n") }.each { |z| z.map!(&:split) }.delete_if(&:empty?)
-p main
-p fxs
+    func_0:
+        mov   b, c
+        inc   c
+        jmp   proc_fib
+
+    print:
+        msg   'Term ', a, ' of Fibonacci series is: ', b        ; output text
+        ret
+    "
+
+binding.pry
 
